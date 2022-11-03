b0b62a7cc930040408b829a4eaae8e2051bc701b
Severo <cmsdof@gmail.com>
2022-11-03 09:30:28 -0300

    Intro ao Node

---
+++ node-js/math.js
@@ -0,0 +1,17 @@
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+const div = (x, y) => x / y;
+const mul = (x, y) => x * y;
+const square = (x) => x * x;
+const PI = 3.14159;
+
+const math ={
+    add: add,
+    sub: sub,
+    div: div,
+    mul: mul,
+    square: square,
+    PI: PI
+}
+
+module.exports = math;
